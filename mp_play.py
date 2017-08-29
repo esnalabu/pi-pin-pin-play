3dc1b54011d04350fd71c76e0b8b3e77cd7fbd76
Anders Myren <esnalabu@hotmail.com>
2017-08-29 18:20:08 +0200

    first working code.

--- mp_play.py
+++ mp_play.py
@@ -13,23 +13,24 @@ GPIO.setup(inputpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Define functions raising and lowering TV-lift
 
-def my_callback(pin):
-    print 'Movment! '
-
 def play_audio():
-    print("Playing audio file ", raiseduration, "s..."  )
+    print("Playing audio file...")
     pygame.mixer.init()
     pygame.mixer.music.load("myFile.wav")
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy() == True:
         continue
-    print("Done playing audio file!"  )
+    print("Done playing audio file!")
 
-# Setting up
+# Start listening for events
 GPIO.add_event_detect(inputpin, GPIO.RISING)
 
 # Run, check for new status, raise/lower on change, sleep for stability
 if __name__ == "__main__":
-    while(True):
-        if GPIO.event_detected(inputpin):
-            print 'Detect!'
+    while True:
+        if GPIO.input(inputpin):
+            print('Input was HIGH')
+            play_audio()
+        else:
+            print('Input was LOW')
+        sleep(1)
